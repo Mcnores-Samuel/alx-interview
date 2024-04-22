31d4e934f3ac1b9b4f5d65881321c94fab35a1bd
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-04-22 02:03:14 +0200

    prints all characters of a Star Wars movie:' '

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,10 +5,13 @@ const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 request(url, async (error, response, body) => {
-  const { characters } = JSON.parse(body);
-  for (const character of characters) {
+  const characters = JSON.parse(body).characters;
+  const promises = characters.map((character) => new Promise((resolve, reject) => {
     request(character, (error, response, body) => {
-      console.log(JSON.parse(body).name);
+      resolve(JSON.parse(body).name);
     });
   }
+  ));
+  const names = await Promise.all(promises);
+  names.forEach((name) => console.log(name));
 });
