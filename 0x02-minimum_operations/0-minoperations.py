e282619252f7a8882579cd4082c347d1017d0f54
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-07-30 01:55:11 +0200

    calculates the fewest number of operations needed to result in exactly n H characters in the file.

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''The minimum operations coding challenge.
+'''
+
+def minOperations(n):
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int) or n <= 1:
+        return 0
+    
+    operations_count = 0
+    current_count = 1
+    clipboard_count = 0
+
+    while current_count < n:
+        if clipboard_count == 0:
+            clipboard_count = current_count
+            current_count += clipboard_count
+            operations_count += 2
+        elif (n - current_count) % current_count == 0:
+            clipboard_count = current_count
+            current_count += clipboard_count
+            operations_count += 2
+        else:
+            current_count += clipboard_count
+            operations_count += 1
+
+    return operations_count
