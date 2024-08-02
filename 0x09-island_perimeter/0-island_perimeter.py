9d9efe9145daa5caf339bcc78240a0050acd8a3c
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-08-02 04:25:21 +0200

    returns the perimeter of the island described in grid:

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,10 +14,10 @@ def island_perimeter(grid):
         int: The perimeter of the island.
     """
     perimeter = 0
-    
+
     rows = len(grid)
     cols = len(grid[0])
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
@@ -33,5 +33,4 @@ def island_perimeter(grid):
                 # Check the right neighbor
                 if j == cols-1 or grid[i][j+1] == 0:
                     perimeter += 1
-    
     return perimeter
